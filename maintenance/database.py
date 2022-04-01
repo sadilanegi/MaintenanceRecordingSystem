9fe60d76e38670b739597295e030afb65b07281d
sadilanegi <sadilanegi@gmail.com>
2022-04-01 22:28:04 +0530

    First Deployment

--- maintenance/database.py
+++ maintenance/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DB_URL ="postgresql://sumit:root@localhost/maintenance_recording"
+SQLALCHEMY_DB_URL ="postgres://fysczrqlhapjuu:0ce46adf36f60066509dc59e0f97116e3309c8250edcaf60209662974d0b3a5c@ec2-52-3-60-53.compute-1.amazonaws.com:5432/d25djsm4dt2pva"
 engine = create_engine(SQLALCHEMY_DB_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
